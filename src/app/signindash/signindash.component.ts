4eba6b4a70374d9ffb9bba2c2543f975e2f74006
Devika488 <devika.ed0116@gmail.com>
2021-12-03 13:21:27 +0530

    email validation

--- src/app/signindash/signindash.component.ts
+++ src/app/signindash/signindash.component.ts
@@ -32,7 +32,7 @@ export class SignindashComponent implements OnInit {
       [
         Validators.required,
         Validators.email,
-        Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-z]{2,4}$'),
+        Validators.pattern('^[a-zA-Z0-9][a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-z]{2,4}$'),
       ],
     ],
     password: [
