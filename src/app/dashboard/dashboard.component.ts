d8d4e1df559f328cb1b71cce62ebff696ea1f209
Devika488 <devika.ed0116@gmail.com>
2021-12-03 13:12:52 +0530

    email validation

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -21,7 +21,7 @@ export class DashboardComponent implements OnInit {
     [
       Validators.required,
       Validators.email,
-      Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-z]{2,4}$'),
+      Validators.pattern('^[a-zA-Z0-9][a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-z]{2,4}$'),
     ]],
     password: ['',[
       Validators.required,
